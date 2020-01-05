5a38b89b9cb8cdc95bcb7247f6862e043711a737
Alex Dale <alex@snowycloud.com>
2020-01-04 18:58:10 -0800

    Created project base header.

---
+++ src/base.h
@@ -0,0 +1,28 @@
+/*
+ * MIDI Controller - Project Base
+ *
+ * Copyright (c) 2020 Alex Dale
+ * This project is licensed under the terms of the MIT license.
+ * See LICENSE for details.
+ */
+#ifndef _MIDI_BASE_H_
+#define _MIDI_BASE_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef bool bool_t;
+
+typedef struct {
+  uint8_t number;
+  union {
+    uint8_t velocity;
+    uint8_t pressure;
+  };
+} note_t;
+
+typedef uint8_t channel_t;
+
+typedef uint16_t pitch_t;
+
+#endif  /* _MIDI_BASE_H_ */
