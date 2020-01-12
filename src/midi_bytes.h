5b7f33ff9b9c4d8da2cef9faa280b8067f472d3c
Alex Dale <alex@snowycloud.com>
2020-01-12 00:09:56 -0800

    Extended MIDI bytes header to work with MIDI words.

--- src/midi_bytes.h
+++ src/midi_bytes.h
@@ -8,8 +8,20 @@
 #ifndef _MIDI_BYTES_H_
 #define _MIDI_BYTES_H_
 
+C_SECTION_BEGIN;
+
 #define MidiIsStatusByte(byte) ((byte) & 0x80)    /* uint8_t -> bool_t */
+
+/* MIDI data byte (7-bit) */
 #define MidiIsDataByte(byte) (!((byte) & 0x80))   /* uint8_t -> bool_t */
+
+/* MIDI data word (14-bit) */
 #define MidiIsDataWord(word) (!((word) & 0xC000))  /* uint16_t -> bool_t */
+uint8_t MidiGetDataWordMsb(uint16_t word);
+uint8_t MidiGetDataWordLsb(uint16_t word);
+bool_t MidiSetDataWordMsb(uint16_t *word, uint8_t msb);
+bool_t MidiSetDataWordLsb(uint16_t *word, uint8_t lsb);
+
+C_SECTION_END;
 
 #endif  /* _MIDI_BYTES_H_ */
