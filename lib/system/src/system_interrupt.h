3f059f24a97e0c7fb6d4c5b41f5e9c8eca52d314
Alex Dale <alex@snowycloud.com>
2020-08-01 04:03:00 -0700

    Implemented disabling interrupts for AVR.

--- lib/system/src/system_interrupt.h
+++ lib/system/src/system_interrupt.h
@@ -8,19 +8,20 @@
 #ifndef _SYSTEM_INTERRUPT_H_
 #define _SYSTEM_INTERRUPT_H_
 
+#ifdef _PLATFORM_AVR
+#include <avr/interrupt.h>
+#define SystemDisableInterrupt() cli()
+#define SystemEnableInterrupt() sei()
+#else  /* else !_PLATFORM_AVR */
 #include "base.h"
-
 C_SECTION_BEGIN;
-
 /* Enables or disable global interrupts.  Does not make any guarentee
  * for non-maskable interrupts.
  * None of these should be called from within an interrupt.
  */
 void SystemDisableInterrupt(void);
 void SystemEnableInterrupt(void);
-
-/* Known types of interrupts. */
-
 C_SECTION_END;
+#endif  /* not _PLATFORM_AVR */
 
 #endif  /* _SYSTEM_INTERRUPT_H_ */
